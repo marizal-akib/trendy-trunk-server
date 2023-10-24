450e9b4b959faaf6f08b3f3c856a427f23f3c4fe
marizal-akib <marizalakib@gmail.com>
2023-10-24 03:06:45 +0100

    add post opparetion

--- index.js
+++ index.js
@@ -29,6 +29,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const brandCollection =client.db('brandDB').collection('products');
+    
+    app.get('/products', async(req,res)=>{
+      const cursor = newProduct.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/products', async(req,res)=>{
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await brandCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
